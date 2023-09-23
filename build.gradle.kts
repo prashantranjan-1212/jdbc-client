045c7e068d233413ccbf1b5b09fb168ff1cc8f5d
prashantranjan-1212 <prashantranjan1212@gmail.com>
2023-09-24 00:13:20 +0530

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.0-M3"
+	id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "dev.prashant"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/dev/prashant/jdbcclient/JdbcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prashant.jdbcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
